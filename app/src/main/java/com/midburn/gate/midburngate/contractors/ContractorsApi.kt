455f973ab43cb885739a339be5f820416a68ed2d
Alex Fourman <alex.fourman@ironsrc.com>
2018-04-09 22:03:40 +0300

    now supporting mock api using ContractorsApi.getMock()

--- app/src/main/java/com/midburn/gate/midburngate/contractors/ContractorsApi.kt
+++ app/src/main/java/com/midburn/gate/midburngate/contractors/ContractorsApi.kt
@@ -5,6 +5,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.POST
 import retrofit2.http.Query
+import retrofit2.mock.BehaviorDelegate
+import retrofit2.mock.MockRetrofit
+import retrofit2.mock.NetworkBehavior
 
 interface ContractorsApi {
 
@@ -19,12 +22,40 @@ interface ContractorsApi {
 
     companion object {
         private val contractorsApi by lazy {
-            Retrofit.Builder()
-                    .baseUrl("https://spark.midburn.org/api/gate/")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build().create(ContractorsApi::class.java)
+            getRetrofit().create(ContractorsApi::class.java)
         }
 
         fun get(): ContractorsApi = contractorsApi
+
+        fun getMock(): ContractorsApi {
+            val retrofit = getRetrofit()
+            // Create a MockRetrofit object with a NetworkBehavior which manages the fake behavior of calls.
+            val behavior = NetworkBehavior.create()
+            val mockRetrofit = MockRetrofit.Builder(retrofit)
+                    .networkBehavior(behavior)
+                    .build()
+            val delegate = mockRetrofit.create(ContractorsApi::class.java)
+            return ContractorsApiMock(delegate)
+        }
+
+        private fun getRetrofit(): Retrofit = Retrofit.Builder()
+                .baseUrl("https://spark.midburn.org/api/gate/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+    }
+
+    private class ContractorsApiMock(private val delegate: BehaviorDelegate<ContractorsApi>) : ContractorsApi {
+        override fun getContractorDetails(barcode: String): Call<Contractor> {
+            return delegate.returningResponse(Contractor("123123", "Israel Israeli")).getContractorDetails(barcode)
+        }
+
+        override fun admitContractor(barcode: String, carPlate: String): Call<Contractor> {
+            return delegate.returningResponse(Contractor("123123", "Israel Israeli")).admitContractor(barcode, carPlate)
+        }
+
+        override fun departContractor(barcode: String, carPlate: String): Call<Contractor> {
+            return delegate.returningResponse(Contractor("123123", "Israel Israeli")).departContractor(barcode, carPlate)
+        }
     }
 }
+
